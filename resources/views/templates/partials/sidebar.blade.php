d73f99b4836b269efb74b41efc5ecee3398ed65f
khalilannbiya <syeichkhalil@gmail.com>
2023-02-16 17:17:15 +0700

    feat: integrate dashboard page and students page

--- resources/views/templates/partials/sidebar.blade.php
+++ resources/views/templates/partials/sidebar.blade.php
@@ -102,7 +102,7 @@
         <div class="collapse navbar-collapse" id="navbar-menu">
             <ul class="navbar-nav pt-lg-3">
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.html">
+                    <a class="nav-link" href="{{ route('dashboard') }}">
                         <span class="nav-link-icon d-md-none d-lg-inline-block">
                             <!-- Download SVG icon from http://tabler-icons.io/i/home -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
